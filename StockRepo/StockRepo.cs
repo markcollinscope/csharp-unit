fefa51598db74ea44a2d479128affc4db700756d
Mark Collins-Cope <mark.collins@epyx.co.uk>
2022-11-15 18:52:04 +0000

    <No-Git-Branch-Available>: basic framework of testing put in, started StoreRepo stuff...

---
+++ StockRepo/StockRepo.cs
@@ -0,0 +1,28 @@
+﻿// package = namespace;
+
+namespace StockRepo;
+
+public class StockItem
+{
+	const int UNDEFINED = -1;
+
+	string Name 			{ get; set; }
+	public int ID  			{ get; set; }
+	public int PricePence 	{ get; set; }
+
+	public StockItem(string name, int pricep)
+	{
+		this.ID = UNDEFINED;
+		this.PricePence = pricep;
+		this.Name = name;
+	}
+}
+
+public class StockRepo
+{
+	public void Clear()
+	{
+ 		throw new NotImplementedException("Not Implemented");
+	}
+}
+
