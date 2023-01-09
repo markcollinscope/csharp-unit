c417f58fd6a52a20cdd0264d591c240a5788fde2
Mark Collins-Cope <markcollinscope@gmail.com>
2023-01-09 22:12:44 +0000

    <master>: Updated with auto-extraction of code for exercises in Makefile; directory restructure

---
+++ EXERCISE-INPUT-SRC-XDIR/EXERCISE-2/StockItem/StockItem.cs
@@ -0,0 +1,19 @@
+﻿// EG-CODE-STOCKITEM ### for extraction.
+namespace StockItemNS;
+
+public class StockItem
+{
+    public int ID			{ get; set; }
+    public string Name 		{ get; set; }
+    public int PricePence 	{ get; set; }
+
+    static private int _nextID = 0;
+
+    public StockItem(string name, int pricep)
+    {
+        this.ID = StockItem._nextID++;
+        this.PricePence = pricep;
+        this.Name = name;
+    }
+}
+// ### END ###
